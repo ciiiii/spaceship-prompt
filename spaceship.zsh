8cbe0da7c21d26ef481be4cb7f226acde20cc81e
ciiiii <allen.ccccnm@gmail.com>
2020-09-27 14:46:42 +0800

    remove unused sections from SPACESHIP_PROMPT_ORDER

--- spaceship.zsh
+++ spaceship.zsh
@@ -46,33 +46,34 @@ if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
     host          # Hostname section
     git           # Git section (git_branch + git_status)
     hg            # Mercurial section (hg_branch  + hg_status)
-    package       # Package version
-    node          # Node.js section
-    ruby          # Ruby section
-    elm           # Elm section
-    elixir        # Elixir section
-    xcode         # Xcode section
-    swift         # Swift section
-    golang        # Go section
-    php           # PHP section
-    rust          # Rust section
-    haskell       # Haskell Stack section
-    julia         # Julia section
+    # package       # Package version
+    # node          # Node.js section
+    # ruby          # Ruby section
+    # elm           # Elm section
+    # elixir        # Elixir section
+    # xcode         # Xcode section
+    # swift         # Swift section
+    # golang        # Go section
+    # php           # PHP section
+    # rust          # Rust section
+    # haskell       # Haskell Stack section
+    # julia         # Julia section
     docker        # Docker section
-    aws           # Amazon Web Services section
-    gcloud        # Google Cloud Platform section
+    # aws           # Amazon Web Services section
+    # gcloud        # Google Cloud Platform section
     venv          # virtualenv section
-    conda         # conda virtualenv section
+    # conda         # conda virtualenv section
     pyenv         # Pyenv section
-    dotnet        # .NET section
-    ember         # Ember.js section
+    # dotnet        # .NET section
+    # ember         # Ember.js section
+    git_user
     kubectl       # Kubectl context section
-    terraform     # Terraform workspace section
+    # terraform     # Terraform workspace section
     exec_time     # Execution time
     line_sep      # Line break
     battery       # Battery level and status
-    vi_mode       # Vi-mode indicator
-    jobs          # Background jobs indicator
+    # vi_mode       # Vi-mode indicator
+    # jobs          # Background jobs indicator
     exit_code     # Exit code section
     char          # Prompt character
   )
